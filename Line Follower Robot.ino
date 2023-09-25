764fc8b48f159c02b6804089b5cba9bc45214a10
djsmanchanda <68808848+djsmanchanda@users.noreply.github.com>
2023-09-26 00:11:26 +0530

    Updated Line Follower Robot.ino

--- Line Follower Robot.ino
+++ Line Follower Robot.ino
@@ -28,7 +28,7 @@ int lsp, rsp;
 int lfspeed = 200;
 
 float Kp = 0;
-float Kd = 0;
+float Kd = 0;// Initializing PWM Values
 float Ki = 0 ;
 
 
@@ -37,8 +37,8 @@ int minValues[8], maxValues[8], threshold[8];
 void setup()
 {
   Serial.begin(9600);
-  pinMode(11, INPUT_PULLUP);
-  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);// button to calibrate the bot
+  pinMode(12, INPUT_PULLUP);// button to start the bot post calibration
   pinMode(13, OUTPUT);
   digitalWrite(13, HIGH);
 
@@ -47,48 +47,54 @@ void setup()
 
 void loop()
 {
-  while (digitalRead(11)) {}
+  while (digitalRead(11)) {}//Calibrating the bot
   delay(1000);
   calibrate();
-  while (digitalRead(12)) {}
+  while (digitalRead(12)) {}//starts bot
   delay(1000);
 
   while (1)
   {
     if ((analogRead(0) > threshold[0] && analogRead(1) < threshold[1] && analogRead(2) > threshold[2]) || (analogRead(0) > threshold[0] && analogRead(1) < threshold[1] && analogRead(2) < threshold[2] && analogRead(3) > threshold[3]) || (analogRead(0) > threshold[0] && analogRead(1) > threshold[1] && analogRead(2) < threshold[2] && analogRead(3) > threshold[3]))
     {
-      obl();
+      obl();// Obtuse Left Case
     }
     if ((analogRead(7 - 0) > threshold[7 - 0] && analogRead(7 - 1) < threshold[7 - 1] && analogRead(7 - 2) > threshold[7 - 2]) || (analogRead(7 - 0) > threshold[7 - 0] && analogRead(7 - 1) < threshold[7 - 1] && analogRead(7 - 2) < threshold[7 - 2] && analogRead(7 - 3) > threshold[7 - 3]) || (analogRead(7 - 0) > threshold[7 - 0] && analogRead(7 - 1) > threshold[7 - 1] && analogRead(7 - 2) < threshold[7 - 2] && analogRead(7 - 3) > threshold[7 - 3]))
     {
-      obr();
+      obr();// Obtuse Right Case
     }
 
     if (analogRead(0) > threshold[0] && analogRead(7) < threshold[7] )
     {
+      //left 90 turn
       lsp = -25; rsp = lfspeed;
       motor1.drive(0);
       motor2.drive(lfspeed);
     }
 
     else if (analogRead(7) > threshold[7] && analogRead(0) < threshold[0])
-    { lsp = lfspeed; rsp = -25;
+    { 
+      //right 90 turn
+      lsp = lfspeed; rsp = -25;
       motor1.drive(lfspeed);
       motor2.drive(0);
     }
     else if ((analogRead(4) > threshold[4]) || (analogRead(3) > threshold[3]))
     {
+      //Straight with PID tuning
+      //Change KP KD values to tune PID, no Ki required since there is no error from sensor and it only introduces lag, better at lower speeds
       Kp = 0.01* (1000  - analogRead(4));
-      Kd = 20
-      * Kp;
+      Kd = 20*Kp;
 
       linefollow();
     }
+    // if the white line ends it turns around and and gets back on the line
   }
 }
 
 void obl()
 {
+  //function for left obtuse angle turn
   lsp = -25; rsp = lfspeed;
   motor1.drive(-150);
   motor2.drive(150);
@@ -97,6 +103,7 @@ void obl()
 
 void obr()
 {
+  //function for right obtuse angle turn
   lsp = lfspeed; rsp = -25;
   motor1.drive(150);
   motor2.drive(-150);
@@ -106,7 +113,6 @@ void obr()
 void linefollow()
 {
   int error = (analogRead(3) - analogRead(5));
-
   P = error;
   I = I + error;
   D = error - previousError;
@@ -143,10 +149,11 @@ void calibrate()
   }
 
   for (int i = 0; i < 3500; i++)
-  {
+  { 
+    //increase i value if 1-1.5  circle isn't completed;
     motor1.drive(75);
     motor2.drive(-75);
-
+    //let it take 2 seconds to finish a circle, too fast will negatively affect calibration
     for ( int i = 0; i < 8; i++)
     {
       if (analogRead(i) < minValues[i])
